015a00d1283e8a1d426c9c6e2af56c39f2edcf74
ourobin2 <39935342+ourobin2@users.noreply.github.com>
2023-01-09 21:19:59 +0800

    Update inspect_tree.py
    add print_layers

--- inspect_tree.py
+++ inspect_tree.py
@@ -15,6 +15,7 @@ def print_info(tree):
     print("This tree has {} rules".format(len(tree.rules)))
     print("Tree stats: {}".format(tree.compute_result()))
     print("Plottable visualization:\n{}".format(tree.stats_str()))
+    print("Layers info {}".format(tree.print_layers()))
 
 
 def check_classification(tree):
